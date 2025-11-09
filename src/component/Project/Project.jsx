5167176430272a405a68d49df23c1f3a76892edb
Mehedi <maaztech@gmail.com>
2025-11-09 19:10:28 +0600

    project links added

--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
 import "./Project.css"
+import { FaLink } from "react-icons/fa6";
+
 const Project = ({ item }) => {
-    const { id, image, title, description, technologies } = item;
+    const { id, image, title, description, technologies, live_link } = item;
     return (
         <div className="card bg-base-100 border-2 border-cyan-950 shadow-sm p-5 m-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto h-96 flex flex-col">
             <figure className="h-48 sm:h-52 md:h-56 lg:h-60 other-pro-img">
@@ -12,7 +15,16 @@ const Project = ({ item }) => {
                 />
             </figure>
             <div className="card-body px-0 flex-1 flex flex-col">
-                <h2 className="card-title font-bold text-base sm:text-lg md:text-xl">{title}</h2>
+                <div className="flex justify-between items-center">
+                    <h2 className="card-title font-bold text-base sm:text-lg md:text-xl">{title}</h2>
+                    <span>
+                        <Link to={live_link} target="_blank">
+                            <button className="flex items-center gap-2 text-base btn ">
+                                <FaLink /> View Live
+                            </button>
+                        </Link>
+                    </span>
+                </div>
                 <p className="font-light text-sm sm:text-base md:text-lg flex-1">{description}</p>
                 <div className="flex flex-wrap gap-2 mt-2">
                     {technologies.map((tec, idx) => (
